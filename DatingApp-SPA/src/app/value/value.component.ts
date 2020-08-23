7f842ad884db29327ec91ba9d4763034e505798b
Krzysiek <krzysiek.ziach@gmail.com>
2020-08-23 20:20:30 +0200

    End of section 3

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './value.component.html',
   styleUrls: ['./value.component.scss']
 })
+
 export class ValueComponent implements OnInit {
   values: any;
 
@@ -19,7 +20,7 @@ export class ValueComponent implements OnInit {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
     }, error => {
-      console.log(error)
+      console.log(error);
     })
   }
 }
